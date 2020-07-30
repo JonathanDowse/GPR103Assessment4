f401d379cbd08af0eb8bd2bd71dc2b1cd44e9ca7
Jonathan Dowse <jonathan.dowse@mds.torrens.edu.au>
2020-07-30 15:47:10 +1000

    Second Commit: Some Sprites and Class Scripts
    I haven't had much time to work on this assessment up until this point, so I intend to dedicate a sizable chunk of time and effort to this project in the coming week in order to make up for time I have missed so far. I have spent time on the theoretical function of a frogger game and brainstorming using pseudocode to make sure I feel confident for the upcoming work I do.

---
+++ Assets/SaveandLoad.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveandLoad : MonoBehaviour
+{
+
+
+    public int playerHighScore = 0;
+    public int playerCurrentScore = 0;
+
+    public string playersName = "DefaultName";
+
+    public float gameTimer = 5f;
+    public bool isGameOver = false;
+
+    public static string PLAYERNAMESAVE = "PlayerName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHighScore = PlayerPrefs.GetInt("HighScore");
+        playersName = PlayerPrefs.GetString(PLAYERNAMESAVE);
+        print("Current High Score: " + playerHighScore);
+        print("Players Name: " + playersName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            playersName = "BingBong";
+            PlayerPrefs.SetString(PLAYERNAMESAVE, playersName);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.Delete))
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGameOver != true)
+        {
+            playerCurrentScore += 1;
+        }
+
+        gameTimer -= Time.deltaTime;
+        if (gameTimer <= 0 && isGameOver != true)
+        {
+            isGameOver = true;
+            if(playerCurrentScore > playerHighScore)
+            {
+                playerHighScore = playerCurrentScore;
+                PlayerPrefs.SetInt("HighScore", playerHighScore);
+            }
+            //Game Over
+        }
+    }
+}
