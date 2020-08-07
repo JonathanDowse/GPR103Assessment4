1811cce6408bf36c60525d28c42c9917f5c7027e
Jonathan Dowse <jonathan.dowse@mds.torrens.edu.au>
2020-08-07 13:46:24 +1000

    Fourth Commit: New & Updated Scripts and New Sprites
    A lot of work has been done on the project in the last few days

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEditor;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    /// <summary>
+    /// -1 = left, 1 = right
+    /// </summary>
+    public int moveDirection = -1;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Initialise(Vector3 position, float speed, float maxX = 21)
+    {
+        transform.position = startingPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
+        float posX = transform.position.x;
+
+        if (posX > 21 || posX < -21)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
